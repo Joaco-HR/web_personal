bac0053c2f8f8b0b47d21665207328abeda88267
“Joaco-HR” <joaquin.h.r.flash@gmail.com>
2025-08-28 22:50:34 -0300

    Ultimo commit

--- web/blog/forms.py
+++ web/blog/forms.py
@@ -4,12 +4,12 @@ class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Your Name"}
+            attrs={"class": "form-control", "placeholder": "Escribe tu nombre"}
         ),
     )
     body = forms.CharField(
         max_length= 2000,
         widget=forms.Textarea(
-            attrs={"class": "form-control", "placeholder": "Leave a comment!"}
+            attrs={"class": "form-control", "placeholder": "Deja un comentario"}
         )
     )
